e13a09872666e01f77404264be81b4db714ad270
nowrd2xpln <allen.mamaril@gmail.com>
2019-05-19 14:02:35 -0700

    Add script cmd line argument handling

--- securesign.sh
+++ securesign.sh
@@ -3,5 +3,39 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo $DIR
+input=""
+output=""
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -i|--input)
+    INPUT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--output)
+    OUTPUT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo ${INPUT}
+echo ${OUTPUT}
+echo ${DEFAULT}
+echo ${POSITIONAL}
 
 echo "GOOD BYE"
